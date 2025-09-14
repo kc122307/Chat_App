6594d34f2cf5f92435d5cfc8e883dbf8cda6529a
kc122307 <kunalchandel1223@gmail.com>
2025-09-14 19:21:39 +0530

    completed

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -32,7 +32,7 @@ const SearchInput = () => {
 			<input
 				type='text'
 				placeholder='Search…'
-				className='input input-bordered rounded-full w-40 md:w-80'
+				className='input input-bordered rounded-full w-80 md:w-40'
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 			/>
